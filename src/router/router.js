1052314af37d25faddc30435c0f3ec43977b46c8
Goddo-ro <r0manovic1@yandex.ru>
2023-07-15 12:12:39 +0300

    Change error page with redirecting

--- src/router/router.js
+++ src/router/router.js
@@ -2,20 +2,16 @@ import Login from "../pages/Login";
 import Posts from "../pages/Posts";
 import NewPost from "../pages/NewPost";
 import Post from "../pages/Post/Post";
-import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import { Navigate } from "react-router-dom";
 
 export const routes = [
     { path: "/login", element: <Login/> },
-    { path: "/", element: <Navigate replace to="login"/>},
-    { path: "*", element: <ErrorPage/> },
+    { path: "*", element: <Navigate replace to="login"/>},
 ];
 
 export const privateRoutes = [
     { path: "/posts", element: <Posts/> },
     { path: "/posts/add", element: <NewPost/> },
     { path: "/posts/:id", element: <Post/> },
-    { path: "/login", element: <Navigate replace to="/posts"/> },
-    { path: "/", element: <Navigate replace to="/posts"/> },
-    { path: "*", element: <ErrorPage/> },
+    { path: "*", element: <Navigate replace to="/posts"/> },
 ]
